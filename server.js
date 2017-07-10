ac7170e715b47b0afe0fd24398d5477521618d94
Aleksandar Grbic <agrbicpp@gmail.com>
2017-07-10 17:26:16 +0200

    trigerring travis CI build

--- server.js
+++ server.js
@@ -6,3 +6,16 @@ app.get('/home', (req, res, next) => {
 });
 
 app.listen(process.env.PORT || 5000, () => console.log('Express server!'));
+
+
+/*
+
+    Created a simple express app
+    Created a git repo
+    Created a simple .yml file
+    Created a simple test
+    pushed stuff to github
+    connected repo with travis
+    we used preinstall under scripts in package.json
+
+*/
